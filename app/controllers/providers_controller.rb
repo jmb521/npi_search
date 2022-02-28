2cff50bf523f687580772778d53abf9683a3867b
Jennifer Pazos <jmp521@gmail.com>
2022-02-27 20:36:45 -0600

    created index action with search logic

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,2 +1,17 @@
 class ProvidersController < ApplicationController
+
+    def index
+        if params[:number] && params[:number].to_i != 0
+            p = Provider.find_by(npi: params[:number])
+            if !p
+                created_provider = Provider.get_provider(params[:number])
+                if !created_provider
+                    @error = "Provider not located"
+                end
+            end
+        elsif params[:number] == " "
+            @error = "Please enter a number"
+        end
+        @providers = Provider.all.order_by_created_at
+    end
 end
