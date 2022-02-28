a4f727f106efdfd58fa0b430246c626f6ead6faf
Jennifer Pazos <jmp521@gmail.com>
2022-02-27 20:37:17 -0600

    upsed js to move searched provider row to top of table

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("submit", (e) => {
+  errorMessage()
+  moveProviderToTop(e)
+})
+
+function errorMessage() {
+  const error = document.querySelector(".error")
+  if(error) {
+    error.remove()
+  }
+}
+
+function moveProviderToTop(e) {
+  const tr = document.querySelector(`[data-id="${e.target[0].value}"]`)
+  if(tr) {
+    e.preventDefault()
+    const tbody = document.querySelector("tbody")
+    tbody.insertBefore(tr, tbody.children[0])
+    document.querySelector("form").reset()
+  }
+  
+}
+
